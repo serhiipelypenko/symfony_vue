93bab1f1fc77b3b9e06de479f571c666d585201b
serhiipelypenko <sergey.pelipenko@triogeeks.com>
2025-09-24 18:11:33 +0300

    product page and category page

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CategoryController extends AbstractController
+{
+    #[Route('/category/{slug:category}', name: 'main_category_show')]
+    public function show(Category $category = null): Response
+    {
+        if(!$category){
+            throw new NotFoundHttpException();
+        }
+        $products = $category->getProducts()->getValues();
+        return $this->render('main/category/show.html.twig', [
+            'category' => $category,
+            'products' => $products
+        ]);
+    }
+}
---
+++ src/Controller/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ProductController extends AbstractController
+{
+    #[Route('/product/{uuid:product}', name: 'main_product_show')]
+    public function show(Product $product = null): Response
+    {
+        if(!$product){
+            throw new NotFoundHttpException();
+        }
+        return $this->render('main/product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+}
